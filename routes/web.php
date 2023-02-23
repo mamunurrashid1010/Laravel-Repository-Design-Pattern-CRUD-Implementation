811aabfc76f9254a0628d1804ad60ccec9c58f43
Muhammed Mamunur Rashid <mamunurrashid1010@gmail.com>
2023-02-23 11:40:36 +0600

    Route create for category CRUD operation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Category
+Route::group(['prefix'=>'category'],function (){
+    Route::get('index',[CategoryController::class,'index'])->name('category.index');
+    Route::get('create',[CategoryController::class,'create'])->name('category.create');
+    Route::post('store',[CategoryController::class,'store'])->name('category.store');
+    Route::get('edit/{id}',[CategoryController::class,'edit'])->name('category.edit');
+    Route::post('update',[CategoryController::class,'update'])->name('category.update');
+    Route::get('show/{id}',[CategoryController::class,'show'])->name('category.show');
+    Route::post('delete/{id}',[CategoryController::class,'delete'])->name('category.delete');
 });
