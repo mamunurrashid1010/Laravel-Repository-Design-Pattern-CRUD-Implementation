db8b3c7489cdd8aa20b023aba72d82643cc344db
Muhammed Mamunur Rashid <mamunurrashid1010@gmail.com>
2023-02-25 11:53:57 +0600

    Category CRUD functionalities create  in CategoryRepositoryInterface.php and CategoryRepository.php

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -1,7 +1,30 @@
 <?php
 
 use App\Repositories\Interfaces\CategoryRepositoryInterface;
+use App\Models\Category;
 
 class CategoryRepository implements CategoryRepositoryInterface{
 
+    public function allCategories(){
+        return Category::all();
+    }
+
+    public function storeCategory($data){
+        return Category::create($data);
+    }
+
+    public function findCategory($id){
+        return Category::find($id);
+    }
+
+    public function updateCategory($data, $id){
+        $category = Category::where('id',$id)->first();
+        $category->name = $data['name'];
+        $category->save();
+    }
+
+    public function destroyCategory($id){
+        $category = Category::find($id);
+        $category->delete();
+    }
 }
--- app/Repositories/Interfaces/CategoryRepositoryInterface.php
+++ app/Repositories/Interfaces/CategoryRepositoryInterface.php
@@ -3,4 +3,10 @@ namespace App\Repositories\Interfaces;
 
 Interface CategoryRepositoryInterface{
 
+    public function allCategories();
+    public function storeCategory($data);
+    public function findCategory($id);
+    public function updateCategory($data, $id);
+    public function destroyCategory($id);
+
 }
