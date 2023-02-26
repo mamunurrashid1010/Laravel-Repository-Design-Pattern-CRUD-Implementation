e19e4176db6bb29e03bf16e1760345989e9a28e6
Muhammed Mamunur Rashid <mamunurrashid1010@gmail.com>
2023-02-26 11:10:36 +0600

    CategoryController : implement method for CRUD operation and create blade file

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,9 +3,56 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Interfaces\CategoryRepositoryInterface;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    private $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    // create
+    public function create(){
+        return view('categories.create');
+    }
+
+    // store
+    public function store(Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+        ]);
+        $this->categoryRepository->storeCategory($data);
+        return redirect()->route('category.index')->with('message', 'create successfully');
+    }
+
+    // show
+    public function show($id){
+        $category = $this->categoryRepository->findCategory($id);
+        return view('categories.details', compact('category'));
+    }
+
+    // edit
+    public function edit($id){
+        $category = $this->categoryRepository->findCategory($id);
+        return view('categories.edit', compact('category'));
+    }
+
+    // update
+    public function update(Request $request, $id){
+        $data = $request->validate([
+            'name' => 'required',
+        ]);
+        $this->categoryRepository->updateCategory($data, $id);
+        return redirect()->route('categories.index')->with('message','update successfully');
+    }
+
+    // delete
+    public function delete($id){
+        $this->categoryRepository->destroyCategory($id);
+        return redirect()->route('categories.index')->with('message', 'Category Delete Successfully');
+    }
 }
